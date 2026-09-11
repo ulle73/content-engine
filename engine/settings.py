aec879baa9e6ee10bbc4236fa8cfc1844fe4a287
ulle73 <145575457+ulle73@users.noreply.github.com>
2026-09-12 01:18:38 +0200

    Install OAuth provider in Django

--- engine/settings.py
+++ engine/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "oauth2_provider",
     "engine",
     "operator_bridge",
 ]
